10b769e10eaf35c2c60f6e443871b544080599d1
julianasiqueiramelo <juliana15souza@gmail.com>
2024-11-12 12:50:33 -0300

    Add files via upload

---
+++ calculadora_script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+python3 /home/daniel/modulo1/linux/calculadora.py
+
+primeiro_numero = input('Coloque o primeiro número AQUI>>> ')
+primeiro_numero = int(primeiro_numero) # Conversão de String para Inteiro
+print (primeiro_numero)
+
+segundo_numero = input('Coloque o segundo número AQUI>>> ')
+segundo_numero = int(segundo_numero) # Conversão de String para Inteiro
+print (segundo_numero)
+
+soma = primeiro_numero + segundo_numero
+print ("Soma:" , soma)
+
+exponenciacao = primeiro_numero ** segundo_numero
+print ("Exponenciação" , exponenciacao)
+
+subtracao = primeiro_numero - segundo_numero
+print ("Subtração" , subtracao)
+
+multiplicacao = primeiro_numero * segundo_numero
+print ("Multiplicação" , multiplicacao)
+
+if segundo_numero != 0 :
+    divisao = primeiro_numero / segundo_numero
+    print("Divisão:", divisao)
+else:
+    print("Divisão: Não é possível dividir por zero.") # Evitar divisão por zero
